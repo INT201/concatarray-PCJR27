e9a412a67d2a8777691f0bf97f98e607c95a5650
porcha <porcha.por@mail.kmutt.ac.th>
2022-09-10 11:44:18 +0700

    test push

--- concatArray.js
+++ concatArray.js
@@ -1,6 +1,23 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
-  //code here
+ if(array1,array2 === null && array1,array2 === undefined ){
+  return undefined
+ }else if(array1 === null || array1=== undefined||array2 === null ||array2 === undefined){
+    return undefined
+ }
+ else if(array1===[] && array2 === []){
+ return []
+}else if(array1 === []||array1===null||array1===undefined){
+    return array2
+}else if(array2 === []||array2===null||array2===undefined){
+    return array1
 }
+else 
+return array1.concat(array2)
+
+
+}
+console.log(concatArray([5, 10, 15], [2, 4, 6, 7]))
+console.log(undefined, [2, 4, 6, 7])
 module.exports = concatArray
