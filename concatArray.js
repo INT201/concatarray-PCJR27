e10a6a6913dd02b9a9f0333f5e7cf773821743d3
porcha <porcha.por@mail.kmutt.ac.th>
2022-09-10 12:04:17 +0700

    push 2 แก้สลับที่array2ขึ้นก่อนarray1

--- concatArray.js
+++ concatArray.js
@@ -1,23 +1,24 @@
 const { template } = require('@babel/core')
 
 function concatArray(array1, array2) {
- if(array1,array2 === null && array1,array2 === undefined ){
+ if(array1 === null ||array1 === undefined && array2 ===null || array2 ===undefined ){
   return undefined
- }else if(array1 === null || array1=== undefined||array2 === null ||array2 === undefined){
-    return undefined
- }
- else if(array1===[] && array2 === []){
- return []
-}else if(array1 === []||array1===null||array1===undefined){
-    return array2
-}else if(array2 === []||array2===null||array2===undefined){
+ }else if(array1===[] && array2 === []){
+    return array1
+   } else if(array2 === []||array2===null||array2===undefined){
     return array1
 }
+ else if(array1=== []||array1 === null || array1=== undefined){
+    return array2
+ }
+
 else 
 return array1.concat(array2)
 
 
 }
 console.log(concatArray([5, 10, 15], [2, 4, 6, 7]))
-console.log(undefined, [2, 4, 6, 7])
+console.log(concatArray(undefined, [2, 4, 6, 7]))
+console.log(concatArray([2, 4, 6, 7], null))
+console.log(concatArray(undefined, [2, 4, 6, 7]))
 module.exports = concatArray
